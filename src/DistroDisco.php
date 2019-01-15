d5c70cdcb3c9957f0e6b35c5e32a0e7b30412829
Bryan Sharpe <bryansharpe@gmail.com>
2019-01-15 09:32:16 -0800

    Adds base robo and bins.

---
+++ src/DistroDisco.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace DistroDisco;
+
+use Robo\Robo;
+use Robo\Config\Config;
+use Robo\Common\ConfigAwareTrait;
+use Symfony\Component\Lock\Factory;
+use Symfony\Component\Lock\Store\SemaphoreStore;
+use Robo\Runner as RoboRunner;
+use Robo\Application;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Consolidation\AnnotatedCommand\CommandFileDiscovery;
+
+/**
+ * Class DistroDisco
+ * @package DistroDisco
+ */
+class DistroDisco
+{
+
+    use ConfigAwareTrait;
+
+    private $runner;
+
+    const NAME = 'DistroDisco';
+
+    const VERSION = '1.0.0';
+
+    /**
+     * DistroDisco constructor.
+     * @param Config               $config
+     * @param InputInterface|null  $input
+     * @param OutputInterface|null $output
+     */
+    public function __construct(Config $config, InputInterface $input = null, OutputInterface $output = null)
+    {
+
+        // Create application.
+        $this->setConfig($config);
+        $application = new Application(self::NAME, self::VERSION);
+
+        $application->getDefinition()->addOptions([
+            new InputOption(
+                '--yes',
+                '-y',
+                InputOption::VALUE_NONE,
+                'Automatically respond "yes" to all confirmation questions.'
+            ),
+        ]);
+
+        // Create and configure container.
+        $container = Robo::createDefaultContainer($input, $output, $application, $config);
+
+        $discovery = new CommandFileDiscovery();
+        $discovery->setSearchPattern('*Command.php');
+        $commandClasses = $discovery->discover(__DIR__ . '/Commands', '\DistroDisco\Commands');
+
+        // Instantiate Robo Runner.
+        $this->runner = new RoboRunner([]);
+        $this->runner->setContainer($container);
+        $this->runner->registerCommandClasses($application, $commandClasses);
+    }
+
+    /**
+     * @param InputInterface  $input
+     * @param OutputInterface $output
+     * @return int
+     */
+    public function run(InputInterface $input, OutputInterface $output)
+    {
+        // Obtain a lock and exit if the command is already running.
+        $store = new SemaphoreStore();
+        $factory = new Factory($store);
+        $lock = $factory->createLock('distro-disco-command');
+
+        if (!$lock->acquire()) {
+            $output->writeln('The command is already running in another process.');
+
+            return 0;
+        }
+
+        $statusCode = $this->runner->run($input, $output);
+
+        // Specifically release the lock after successful command invocation.
+        $lock->release();
+
+        return $statusCode;
+    }
+}
